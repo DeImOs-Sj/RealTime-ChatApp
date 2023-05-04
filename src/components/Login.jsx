33dde8bc651d710dbd7581c00280e7e8504615dc
Shlok <shlokjagtap123@gmail.com>
2023-05-04 22:20:00 +0530

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <div className='bg-black'>
+            <h1>Django Chat</h1>
+        </div>
+        <div>
+            <form action="">
+
+            </form>
+        </div>
+    </div>
+  )
+}
